86e78fc2077f37d208a67dfa4a056d7a4eab0932
Saoud Rizwan <fcmiapps@gmail.com>
2017-02-12 13:07:29 -0800

    Initial Commit

---
+++ DanceExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DanceExample
+//
+//  Created by Saoud Rizwan on 2/12/17.
+//  Copyright © 2017 Saoud Rizwan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
